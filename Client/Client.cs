f0070dd608a6edd409aa5267ea1a91d8bc528087
bokica777 <matijevicbojan35@gmail.com>
2025-09-10 18:44:51 +0200

    Pocetak servera i prihvatanje igraca kao i pocetne strukture

---
+++ Client/Client.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint destinationEP = new IPEndPoint(IPAddress.Parse("192.168.56.1"), 50001); // Odredisni IPEndPoint, IP i port ka kome saljemo
+
+            Console.WriteLine("Unesite poruku:");
+            string poruka = Console.ReadLine();
+            byte[] binarnaPoruka = Encoding.UTF8.GetBytes(poruka);
+            try
+            {
+                int brBajta = clientSocket.SendTo(binarnaPoruka, 0, binarnaPoruka.Length, SocketFlags.None, destinationEP); // Poruka koju saljemo u binarnom zapisu, pocetak poruke, duzina, flegovi, odrediste
+
+                Console.WriteLine($"Uspesno poslato {brBajta} ka {destinationEP}");
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Doslo je do greske tokom slanja poruke: \n{ex}");
+            }
+
+            Console.WriteLine("Klijen zavrsava sa radom");
+            clientSocket.Close(); // Zatvaramo soket na kraju rada
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 50001); // Serverov IPEndPoint, IP i port na kom ce server soket primati poruke
+            serverSocket.Bind(serverEP); // Povezujemo serverov soket sa njegovim EP
+            Console.WriteLine($"Server je pokrenut i ceka poruku na: {serverEP}");
+
+            EndPoint posiljaocEP = new IPEndPoint(IPAddress.Any, 0); // Serverov IPEndPoint, IP i port na kom ce server soket primati poruke
+
+            byte[] prijemniBafer = new byte[1024]; // Inicijalizujemo bafer za prijem podataka sa pretpostavkom da poruka nece biti duza od 1024 bajta
+            try
+            {
+                int brBajta = serverSocket.ReceiveFrom(prijemniBafer, ref posiljaocEP); // Primamo poruku i podatke o posiljaocu
+                string poruka = Encoding.UTF8.GetString(prijemniBafer, 0, brBajta);
+                Console.WriteLine($"Stiglo je {brBajta} od {posiljaocEP}, poruka:\n{poruka}");
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Doslo je do greske tokom prijema poruke: \n{ex}");
+            }
+
+            Console.WriteLine("Server zavrsava sa radom");
+            serverSocket.Close(); // Zatvaramo soket na kraju rada
+            Console.ReadKey();
+        }
+    }
+}
